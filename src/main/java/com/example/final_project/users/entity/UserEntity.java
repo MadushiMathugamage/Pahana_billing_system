b06a6ea4c716f365a2ef3cb3082ca3d75ad62983
Madushi <madushimathugamage>
2025-08-06 20:50:01 +0530

    feat: add UserEntity class with basic fields and role assignment

---
+++ src/main/java/com/example/final_project/users/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package com.example.final_project.users.entity;
+
+import com.example.final_project.basic.Role;
+
+public class UserEntity {
+    private int user_id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
