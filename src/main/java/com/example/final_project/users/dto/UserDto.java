4f0a77cf4aa48d48e8bb85ac6177c40bacce5485
Madushi <madushimathugamage>
2025-08-07 11:51:58 +0530

    feat: add UserDto class implementing SuperDto with user details and role

---
+++ src/main/java/com/example/final_project/users/dto/UserDto.java
@@ -0,0 +1,43 @@
+package com.example.final_project.users.dto;
+
+import com.example.final_project.common.base.SuperDto;
+import com.example.final_project.common.basic.Role;
+
+public class UserDto implements SuperDto {
+    private int user_id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
