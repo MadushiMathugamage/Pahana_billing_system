4519f1c06f6c56e8f8d1ac4b9f9b62e14ac8538f
Madushi <madushimathugamage>
2025-08-10 19:27:03 +0530

    feat: extend CrudService interface with add, getAll, update, searchById, and delete methods

--- src/main/java/com/example/final_project/common/base/CrudService.java
+++ src/main/java/com/example/final_project/common/base/CrudService.java
@@ -1,4 +1,17 @@
 package com.example.final_project.common.base;
 
-public interface CrudService {
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public interface CrudService <T extends SuperDto> extends SuperService{
+    boolean add(T dto) throws SQLException, ClassNotFoundException;
+
+
+    List<T> getAll(Map<String, String> searchParams) throws SQLException, ClassNotFoundException;
+
+    boolean update(T dto) throws SQLException, ClassNotFoundException;
+    T searchById(Object... args) throws SQLException, ClassNotFoundException;
+    boolean delete(Object... args) throws SQLException, ClassNotFoundException;
+
 }
