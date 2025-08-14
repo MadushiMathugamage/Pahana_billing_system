f2c76cbe5619fa7042ab58878ed4c35404a59d78
Madushi <madushimathugamage>
2025-08-14 20:14:12 +0530

    chore: add CrudDao

--- src/main/java/com/example/final_project/common/base/CrudDao.java
+++ src/main/java/com/example/final_project/common/base/CrudDao.java
@@ -1,4 +1,20 @@
 package com.example.final_project.common.base;
 
-public interface CrudDao {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public interface CrudDao<T extends SuperEntity> extends SuperDao{
+    boolean add(Connection connection, T entity) throws SQLException, ClassNotFoundException;
+
+    List<T> getAll(Connection connection, Map<String, String> searchParams) throws SQLException, ClassNotFoundException;
+
+    boolean update(Connection connection, T entity) throws SQLException, ClassNotFoundException;
+
+    boolean delete(Connection connection, Object... args) throws SQLException, ClassNotFoundException;
+
+    T searchById(Connection connection, Object... args) throws SQLException, ClassNotFoundException;
+
+
 }
