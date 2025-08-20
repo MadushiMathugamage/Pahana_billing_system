d51b9c72c5fe33a3a72ea7c68670cd2012c67531
Madushi <madushimathugamage>
2025-08-20 22:52:07 +0530

    chore: add CustomerDto.jsp

--- src/main/java/com/example/final_project/customer/dto/CustomerDto.java
+++ src/main/java/com/example/final_project/customer/dto/CustomerDto.java
@@ -1,73 +1,37 @@
 package com.example.final_project.customer.dto;
 
-import com.example.final_project.common.base.SuperDto;
-
-public class CustomerDto implements SuperDto {
-    private int customer_id;
-    private String account_number;
+public class CustomerDto {
+    private int id;
     private String name;
     private String address;
-    private String telephone;
-    private int units_consumed;
+    private String Telephone;
+    private String code;
 
-    public CustomerDto(int customer_id, String account_number, String name, String address, String telephone, int units_consumed) {
-        this.customer_id = customer_id;
-        this.account_number = account_number;
+    // Constructor
+    public CustomerDto(int id, String name, String address, String phone, String code) {
+        this.id = id;
         this.name = name;
         this.address = address;
-        this.telephone = telephone;
-        this.units_consumed = units_consumed;
+        this.Telephone = phone;
+        this.code = code;
     }
 
     public CustomerDto() {
 
     }
 
-    public int getCustomer_id() {
-        return customer_id;
-    }
 
-    public void setCustomer_id(int customer_id) {
-        this.customer_id = customer_id;
-    }
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhone() { return Telephone; }
+    public String getCode() { return code; }
 
-    public String getAccount_number() {
-        return account_number;
-    }
-
-    public void setAccount_number(String account_number) {
-        this.account_number = account_number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public int getUnits_consumed() {
-        return units_consumed;
-    }
-
-    public void setUnits_consumed(int units_consumed) {
-        this.units_consumed = units_consumed;
-    }
+    // Setters (optional)
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setAddress(String address) { this.address = address; }
+    public void setPhone(String phone) { this.Telephone = phone; }
+    public void setCode(String code) { this.code = code; }
 }
